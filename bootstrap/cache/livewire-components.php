e033aad412322e1d324c40ec5c7f366915aadeef
Nadhilah Zakkya <50857210+nzzakkya@users.noreply.github.com>
2020-11-19 18:37:32 +0700

    bootstrap

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'delete-expired-order' => 'App\\Http\\Livewire\\DeleteExpiredOrder',
+  'main-chat' => 'App\\Http\\Livewire\\MainChat',
+  'mentor-list' => 'App\\Http\\Livewire\\MentorList',
+  'ongoing-order' => 'App\\Http\\Livewire\\OngoingOrder',
+  'pay' => 'App\\Http\\Livewire\\Pay',
+  'request-form' => 'App\\Http\\Livewire\\RequestForm',
+  'request-payment' => 'App\\Http\\Livewire\\RequestPayment',
+  'show-chat' => 'App\\Http\\Livewire\\ShowChat',
+  'show-rating' => 'App\\Http\\Livewire\\ShowRating',
+  'start-order' => 'App\\Http\\Livewire\\StartOrder',
+  'update-profile' => 'App\\Http\\Livewire\\UpdateProfile',
+);
---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,88 @@
+<?php return array (
+  'barryvdh/laravel-debugbar' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\Debugbar\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Debugbar' => 'Barryvdh\\Debugbar\\Facade',
+    ),
+  ),
+  'barryvdh/laravel-dompdf' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\DomPDF\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'PDF' => 'Barryvdh\\DomPDF\\Facade',
+    ),
+  ),
+  'facade/ignition' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Facade\\Ignition\\IgnitionServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Flare' => 'Facade\\Ignition\\Facades\\Flare',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'fruitcake/laravel-cors' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fruitcake\\Cors\\CorsServiceProvider',
+    ),
+  ),
+  'laravel/fortify' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Fortify\\FortifyServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'livewire/livewire' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Livewire\\LivewireServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Livewire' => 'Livewire\\Livewire',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+);
